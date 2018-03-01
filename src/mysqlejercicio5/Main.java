e82fbe4d9e65f4003dc8e5ea8303cecec42129c2
DAW <DAW@10.2.1.34>
2018-03-01 17:52:24 +0100

    1

---
+++ src/mysqlejercicio5/Main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mysqlejercicio5;
+
+/**
+Crear una aplicación que conecte con la BBDD de Tienda y que cree en una
+carpeta llamada “Pedidos” una carpeta para cliente con pedidos
+realizados con el “IdCliente” como nombre de la carpeta.
+Dentro de la carpeta de cada cliente debe existir un fichero de texto (.txt)
+para cada pedido con el número del pedido como nombre y con la
+información del pedido como contenido.
+La información del pedido es número de pedido, fecha, las líneas del
+pedido (nº de línea, nombre del producto, cantidad, precio, y descuento)
+y el importe total.
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
