a2562962f8d5bb3844eaf6679c3ba83bb370380c
DAW <DAW@10.2.1.34>
2018-03-01 19:33:34 +0100

    3

--- src/mysqlejercicio5/Main.java
+++ src/mysqlejercicio5/Main.java
@@ -4,70 +4,80 @@
  * and open the template in the editor.
  */
 package mysqlejercicio5;
+
 import java.sql.*;
 import java.io.*;
 import static mysqlejercicio5.Methods.*;
+
 /**
-Crear una aplicación que conecte con la BBDD de Tienda y que cree en una
-carpeta llamada “Pedidos” una carpeta para cliente con pedidos
-realizados con el “IdCliente” como nombre de la carpeta.
-* 
-Dentro de la carpeta de cada cliente debe existir un fichero de texto (.txt)
-para cada pedido con el número del pedido como nombre y con la
-información del pedido como contenido.
-La información del pedido es número de pedido, fecha, las líneas del
-pedido (nº de línea, nombre del producto, cantidad, precio, y descuento)
-y el importe total + iva
+ * Crear una aplicación que conecte con la BBDD de Tienda y que cree en una
+ * carpeta llamada “Pedidos” una carpeta para cliente con pedidos realizados con
+ * el “IdCliente” como nombre de la carpeta.
+ *
+ * Dentro de la carpeta de cada cliente debe existir un fichero de texto (.txt)
+ * para cada pedido con el número del pedido como nombre y con la información
+ * del pedido como contenido. La información del pedido es número de pedido,
+ * fecha, las líneas del pedido (nº de línea, nombre del producto, cantidad,
+ * precio, y descuento) y el importe total + iva
  */
 public class Main {
-
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         final String LINE = "\t+" + line(50, "-") + line(10, "-") + line(10, "-");
 
         Conexion login = new Conexion();
         Connection con = null;
         PreparedStatement stmt = null;
         PreparedStatement stmt2 = null;
-        
-        try{
+        PreparedStatement stmt3 = null;
+
+        try {
             //conexión a la base de datos
             con = login.conectar();
-            
+
             //crear carpeta Pedidos
             File folderPedidos = new File("Pedidos");
             createFolder(folderPedidos);
-            
+
             //consulta de idcliente
             stmt = con.prepareStatement("select DISTINCT cliente from pedidos ");
             ResultSet rs = stmt.executeQuery();
-            
-            
-            while (rs.next()){
+
+            while (rs.next()) {
                 String nombreFolder = rs.getString(1);
                 File folderIdCliente = new File(folderPedidos, nombreFolder);
                 createFolder(folderIdCliente);
-                
+
                 //consulta para sacar nº de pedido que se usa como nombre de archivo txt 
                 stmt2 = con.prepareStatement("SELECT * FROM pedidos WHERE cliente=?");
                 stmt2.setString(1, nombreFolder);
                 ResultSet rs2 = stmt2.executeQuery();
-                
+
                 File fileIdPedido = null;
-                while (rs2.next()){
-                    fileIdPedido = new File (folderIdCliente,rs2.getString(1)+".txt");
-                    writeFile(fileIdPedido, "prueba");
+                while (rs2.next()) {
+                    String numPedido = rs2.getString(1);
+                    fileIdPedido = new File(folderIdCliente, numPedido + ".txt");
+
+                    stmt3 = con.prepareStatement("SELECT * FROM lineaspedido where NumPedido=?");
+                    stmt3.setString(1, numPedido);
+                    ResultSet rs3 = stmt3.executeQuery();
+                    String text = "";
+                    while (rs3.next()){
+                        text += rs3.getInt(3)+" ";
+                        writeFile(fileIdPedido, text );  
+                    }
+                    
                 }
-                
+
             }
-            
-        }catch (Exception ex){
+
+        } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
     }
-    
+
 }
